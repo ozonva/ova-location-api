9a289d9447ba62034ba2247474479a7426dd3589
optimus_frai <maxx.gulyaev@gmail.com>
2021-08-12 18:00:38 +0300

    Правит логику работу функции MapFlip

--- internal/utils/map.go
+++ internal/utils/map.go
@@ -1,12 +1,10 @@
 package utils
 
-import "fmt"
-
 func MapFlip(sourceMap map[string]string) map[string]string {
 	resultMap := make(map[string]string, len(sourceMap))
 	for key, value := range sourceMap {
 		if _, found := resultMap[value]; found {
-			panic(fmt.Sprintf("Пара с таким ключом уже присутствует в словаре: %v", value))
+			continue
 		}
 		resultMap[value] = key
 	}
