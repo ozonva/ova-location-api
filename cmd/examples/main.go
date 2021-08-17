245cb9117b5643a4d1cdc718b182263e29dfd55b
optimus_frai <maxx.gulyaev@gmail.com>
2021-08-17 17:47:37 +0300

    Добавляет примеры использования

--- cmd/examples/main.go
+++ cmd/examples/main.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/ozonva/ova-location-api/internal/location"
 	"github.com/ozonva/ova-location-api/internal/utils"
+	"time"
 )
 
 func main() {
+	fmt.Println("Читаем файлы")
+	{
+		paths := []string{
+			"/Users/optimusfrai/projects/ozon/ova-location-api/cmd/examples/.config",
+		}
+		fmt.Println(paths)
+		data, _ := utils.ReadFiles(paths)
+		fmt.Println(data)
+	}
 	fmt.Println("Разбиваем слайс на слайсы")
 	{
 		sourceSlice := []string{"a", "b", "c", "d", "e"}
 		fmt.Println(sourceSlice)
 		fmt.Println(utils.SliceSplit(sourceSlice, 2))
+	}
+
+	fmt.Println("Разбиваем слайс локаций на слайсы")
+	{
+		sourceSlice := []location.Location{
+			{1, 1, "Some address 1", 20, 30, time.Now()},
+			{2, 2, "Some address 2", 22, 34, time.Now()},
+		}
+		fmt.Println(sourceSlice)
+		fmt.Println(utils.LocationSliceSplit(sourceSlice, 1))
+	}
+
+	fmt.Println("Превращаем слайс локаций в мапу по Id")
+	{
+		sourceSlice := []location.Location{
+			{1, 1, "Some address 1", 20, 30, time.Now()},
+			{2, 2, "Some address 2", 22, 34, time.Now()},
+		}
+		fmt.Println(sourceSlice)
+		fmt.Println(utils.LocationSliceToMap(sourceSlice))
 	}
 
 	fmt.Println("Фильтруем слайс по слайсу")
