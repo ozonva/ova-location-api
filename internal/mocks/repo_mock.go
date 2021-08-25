883ddce0e97b4a0da22d213cdf9b99a6d18d08ee
optimus_frai <maxx.gulyaev@gmail.com>
2021-08-25 21:16:27 +0300

    Правит интерфейс `repo`

--- internal/mocks/repo_mock.go
+++ internal/mocks/repo_mock.go
@@ -48,19 +48,19 @@ func (mr *MockRepoMockRecorder) AddEntities(arg0 interface{}) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddEntities", reflect.TypeOf((*MockRepo)(nil).AddEntities), arg0)
 }
 
-// DescribeEntity mocks base method.
-func (m *MockRepo) DescribeEntity(arg0 uint64) (*location.Location, error) {
+// GetEntity mocks base method.
+func (m *MockRepo) GetEntity(arg0 uint64) (*location.Location, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "DescribeEntity", arg0)
+	ret := m.ctrl.Call(m, "GetEntity", arg0)
 	ret0, _ := ret[0].(*location.Location)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// DescribeEntity indicates an expected call of DescribeEntity.
-func (mr *MockRepoMockRecorder) DescribeEntity(arg0 interface{}) *gomock.Call {
+// GetEntity indicates an expected call of GetEntity.
+func (mr *MockRepoMockRecorder) GetEntity(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DescribeEntity", reflect.TypeOf((*MockRepo)(nil).DescribeEntity), arg0)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetEntity", reflect.TypeOf((*MockRepo)(nil).GetEntity), arg0)
 }
 
 // ListEntities mocks base method.
