69a8c3614296534363dd0eedc39989d35bbef571
optimus_frai <maxx.gulyaev@gmail.com>
2021-08-25 21:16:27 +0300

    Правит интерфейс `repo`

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -5,5 +5,5 @@ import "github.com/ozonva/ova-location-api/internal/location"
 type Repo interface {
 	AddEntities(entities []location.Location) error
 	ListEntities(limit, offset uint64) ([]location.Location, error)
-	DescribeEntity(entityId uint64) (*location.Location, error)
+	GetEntity(entityId uint64) (*location.Location, error)
 }
